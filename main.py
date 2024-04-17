adad16059fb1c6516bc2212ef95805c35d116f44
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-04-17 11:06:19 +0200

    splitting content of loaded text file added

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 from dotenv import load_dotenv
 
 #load environment variables
 load_dotenv()
 
+#initialize text splitter
+text_splitter = CharacterTextSplitter(
+    separator = "\n",                           #split on this character
+    chunk_size = 200,                            #maximum chunk size
+    chunk_overlap = 0
+)
+
 #read text file
 loader = TextLoader("facts.txt")
-docs = loader.load()
+docs = loader.load_and_split(
+    text_splitter = text_splitter
+    )
 
-print(docs)
+
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
