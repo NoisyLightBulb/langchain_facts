56e9fd49352049c1702b092b446662e847406de1
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-04-17 11:11:53 +0200

    embeddings initialized

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OctoAIEmbeddings
 from dotenv import load_dotenv
 
 #load environment variables
 load_dotenv()
 
+#initialize embeddings
+embeddings = OpenAIEmbeddings()
+
 #initialize text splitter
 text_splitter = CharacterTextSplitter(
     separator = "\n",                           #split on this character
-    chunk_size = 200,                            #maximum chunk size
+    chunk_size = 200,                           #maximum chunk size
     chunk_overlap = 0
 )
 
