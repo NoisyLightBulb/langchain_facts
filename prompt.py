13e7e3410928412ea618631275f2d2c9f20fe657
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-04-17 15:19:17 +0200

    initializing retriever and creating chain

--- prompt.py
+++ prompt.py
@@ -1,5 +1,6 @@
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
+from langchain.chains import RetrievalQA
 from dotenv import load_dotenv
 
 #load environment variables
@@ -12,4 +13,14 @@ embeddings = OpenAIEmbeddings()
 db = Chroma(
     persist_directory = "emb",
     embedding_function = embeddings
+)
+
+#initializing retriever
+retriever = db.as_retriever()
+
+#creating chain
+chain = RetrievalQA.from_chain_type(
+    llm = chat,
+    retriever = retriever,
+    chain_type = "stuff"
 )
