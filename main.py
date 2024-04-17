17bb02dcedd64d4301d04beeac9cb639434c7168
Florian Hinterleitner <florian.hinterleitner@gmail.com>
2024-04-17 15:06:15 +0200

    vector db added

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OctoAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
 
 #load environment variables
@@ -18,11 +19,27 @@ text_splitter = CharacterTextSplitter(
 
 #read text file
 loader = TextLoader("facts.txt")
-docs = loader.load_and_split(
-    text_splitter = text_splitter
+docs = loader.load_and_split(text_splitter = text_splitter)
+
+#initialize Chroma vector database
+#and calculate embeddings
+db = Chroma.from_documents(
+    docs, #
+    embedding = embeddings,
+    persist_directory = "emb"
     )
 
 
-for doc in docs:
-    print(doc.page_content)
+results = db.similarity_search_with_score(
+    "What is an interesting fact about the English language",
+    k=3                                                                 #number of results
+    )
+
+# results without score
+# results = db.similarity_search("What is an interesting fact about large mammals")
+
+
+for result in results:
     print("\n")
+    print(result[1])
+    print(result[0].page_content)
